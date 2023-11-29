cbf908a199765537b5c9bb1aece4bbee59b1aeae
ianstlaurent <ianstlaurent7@gmail.com>
2023-11-29 12:48:58 -0800

    updated DEM

--- river_change_analysis/dem_extraction.py
+++ river_change_analysis/dem_extraction.py
@@ -1,11 +1,17 @@
 #Python Script to download DEMs from Google Earth Engine
 import ee
 
-def retrieve_dem(roi):
-    srtm = ee.Image('USGS/SRTMGL1_003')
-    dem = srtm.clip(roi)
+srtm = ee.Image('USGS/SRTMGL1_003')
+class dem:
+    def __init__(roi):
+        dem = srtm.clip(roi)
+        elevation = dem.select('elevation')
+        slope = ee.Terrain.slope(elevation)
 
-    return dem
+
+
+
+
 
 
 
