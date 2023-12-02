a1fe04bea2bcfdd2460c6cb391170c7465aa9d59
ianstlaurent <ianstlaurent7@gmail.com>
2023-12-02 11:39:21 -0800

    Update GEE extraction

--- river_change_analysis/gee_extraction.py
+++ river_change_analysis/gee_extraction.py
@@ -160,6 +160,17 @@ def process_images(start_year, end_year, month_day_start, month_day_end, roi, fo
         noise_removal_p50 = active_p50.updateMask(active_p50.connectedPixelCount(cleaning_pixels, False).gte(cleaning_pixels)).unmask(smooth_map_p50)
         noise_removal_p50_Masked = noise_removal_p50.updateMask(noise_removal_p50.gt(0))
 
+
+        # IMPORT AND VISUALIZE SURFACE WATER MASK.
+        # Surface water occurrence dataset from the JRC (Pekel et al., 2016).
+        jrcYearly = ee.ImageCollection('JRC/GSW1_3/YearlyHistory')
+
+        # Select the seasonal and permanent pixels image representing the year 2000
+        watermask_jrc = jrcYearly.filter(ee.Filter.eq('year', year)).first() \
+        .gte(2).unmask(0) \
+        .clip(roi)
+
+
         # Remove sand bars and other features from the river mask
         watermask = noise_removal_p50_Masked.focal_max().focal_min()
         MIN_SIZE = 2E3
@@ -176,13 +187,14 @@ def process_images(start_year, end_year, month_day_start, month_day_end, roi, fo
         # Next identify the river from the surrounding water bodies
 
         costmap = filled.Not().cumulativeCost(
-        source=watermask.And(ee.Image().byte().paint(sword, 1)),
+        source= watermask.And(watermask_jrc.ee.Image().byte().paint(sword, 1)),
         maxDistance=3E3,
         geodeticDistance=False
         )
 
-        rivermask = costmap.eq(0).rename('riverMask')
-        active_channel_mask = rivermask.And(watermask)
+        wetted_rivermask = costmap.eq(0).rename('riverMask')
+        wetted_rivermask= wetted_rivermask.And(watermask)
+
 
         # Define outputs:
         Wetted_channel = waterMasked_p50
@@ -192,7 +204,7 @@ def process_images(start_year, end_year, month_day_start, month_day_end, roi, fo
 
         filename = file_name + str(year)
         task = ee.batch.Export.image.toDrive(
-            image = active_channel_mask,
+            image = wetted_rivermask,
             description = filename,
             fileNamePrefix = file_name + str(year),
             region = roi.getInfo()['coordinates'],
