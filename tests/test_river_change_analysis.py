527b73538f66e04137972f9277d7ff73fa6fe200
Ian St. Laurent <93612959+ianstlaurent@users.noreply.github.com>
2023-12-04 17:39:09 -0800

    Update test_river_change_analysis.py

--- tests/test_river_change_analysis.py
+++ tests/test_river_change_analysis.py
@@ -1,29 +1,23 @@
 #!/usr/bin/env python
 import sys
 """Tests for `river_change_analysis` package."""
-#sys.path.append("/Users/ian/Desktop/School/Fall 2023/CSC 497/Python_Library/river_change_analysis/river_change_analysis")
 import river_change_analysis as rca
-#print(rca.__path__)
-#rca.authenticate_gee()
-# Define the path to the folder where the binary river masks are stored.
+
 folder_path = "CSC_497"
 dem_folder_path = "/content/drive/MyDrive/ content drive MyDrive CSC_497"
-# Define the pattern that the filenames of the binary river masks follow.
 file_pattern = "Reach_1_"
 file_pattern_2 = "Reach_2_"
 dem_file_pattern = "Athabasca"
 date_start = '-06-01'
 date_end = '-10-01'
 
-year_start = 2013
-year_end = 2015
+year_start = 1986
+year_end = 2021
 
 region = rca.define_roi([])
 
-#rca.process_images(year_start, year_end, date_start, date_end,region, folder_path, file_pattern)
+rca.process_images(year_start, year_end, date_start, date_end,region, folder_path, file_pattern)
 
-
-# Now you can use mask_import and YearlyRiverAnalysis through the rc alias
 rivers_files = rca.mask_import('/Users/ian/Desktop/School/Fall 2023/CSC 497/Python_Library/rca/binary_river_masks/', 'Active_channel_binary_mask_python_')
 
 # Process each year and store in a list
